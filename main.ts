b508312ec484ee954b7450271eb6f5ce331cceae
Irfan Hussain <I.hussain@expressmedia.int>
2024-05-05 16:33:46 +0500

    simple calculator ya phir ATM Machine

---
+++ main.ts
@@ -0,0 +1,25 @@
+#!/usr/bin/env node
+
+import inquirer from "inquirer";
+
+let number = await inquirer.prompt ([{
+name :"firstNumber",
+message: " your first number",
+type: "number"},
+
+{name :"secondNumber",
+message: " your second number",
+type: "number"},
+{name : "option",
+message: "selec one of the operation",
+type: "list",
+choices: ["add", "del","multi","div"]
+}]);
+if (number.option === "add") 
+    {console.log (number.firstNumber + number.secondNumber)}
+else if (number.option === "del") 
+    {console.log (number.firstNumber - number.secondNumber)}
+else if (number.option === "multi") 
+    {console.log (number.firstNumber * number.secondNumber)}
+else if (number.option === "div") 
+    {console.log (number.firstNumber / number.secondNumber)}
